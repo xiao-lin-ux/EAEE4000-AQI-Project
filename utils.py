ff544d4012d219f6b02686d5e1b6f0b493713cf2
Xiao Lin <xl3356@columbia.edu>
2023-11-29 10:39:13 +0000

    Fixed the graphs in NN notebook

--- utils.py
+++ utils.py
@@ -15,9 +15,26 @@ file_dir = './aqi_dataset.csv'
 def df_prep(file):
     df = pd.read_csv(file)
     # Delete unnecessary columns
-    df = df.drop(columns=['Country', 'AQI Category', 'CO AQI Category', 'Ozone AQI Category', 'NO2 AQI Category', 'PM2.5 AQI Category'])
-    
-    # WIP: normalization of the data
-    
-    return df
-    
+    df = df.drop(columns=['Country', 'City', 'AQI Category', 'CO AQI Category', 'Ozone AQI Category', 'NO2 AQI Category', 'PM2.5 AQI Category'])
+    # Randomly shuffle the rows
+    df = df.sample(frac=1, random_state=42).reset_index(drop=True)
+    # Split training set and test set with 8-2 ratio
+    train = df.sample(frac=0.8,random_state=42)
+    test = df.drop(train.index)
+    train = train.reset_index(drop=True)
+    test = test.reset_index(drop=True)
+    return train, test
+
+def make_dir(path):
+    if os.path.exists(path) is False:
+        os.makedirs(path)
+        
+def plot_history(history):
+    plt.figure()
+    plt.xlabel('Epoch')
+    plt.ylabel('Mean squared error')
+    plt.plot(history.epoch, np.array(history.history['loss']),
+           label='Train Loss')
+    plt.plot(history.epoch, np.array(history.history['val_loss']),
+           label = 'Val loss')
+    plt.legend()
